692ab829d6214d2e337cbaee94d94b6fa3732078
Ali Eissa <aeissa@Alis-MacBook-Air.local>
2026-03-30 08:51:26 -0400

    Remove unused imports.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import { Users } from './Users'
